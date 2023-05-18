cf5bb01531f96836d575397e913c706682257f3a
Marina Iurchenko <lamanch1203@gmail.com>
2023-05-18 13:44:25 +0300

    construct changes

--- src/main/java/radiostation/Radio.java
+++ src/main/java/radiostation/Radio.java
@@ -4,12 +4,24 @@ public class Radio {
     private int minRadioVolume = 0;
     private int maxRadioVolume = 100;
     private int autoRadioVolume = minRadioVolume;
-
-
     private int minNumberStation = 0;
+    private int maxNumberStation = 9;
     private int excessNumberStation = 10;
-    private int maxNumberStation = excessNumberStation - 1;
+
     private int currentNumberStation = minNumberStation;
+
+    public Radio(int excessNumberStation) {
+        if (excessNumberStation < minNumberStation) {
+            return;
+        } else {
+            this.excessNumberStation = excessNumberStation;
+        }
+        this.maxNumberStation = excessNumberStation;
+    }
+
+    public Radio() {
+
+    }
 
     public int getMaxRadioVolume() {
         return maxRadioVolume;
@@ -39,17 +51,5 @@ public class Radio {
         return currentNumberStation;
     }
 
-    public Radio(int excessNumberStation) {
-        if (excessNumberStation < minNumberStation) {
-            return;
-        } else {
-            this.excessNumberStation = excessNumberStation;
-        }
-        this.maxNumberStation = excessNumberStation;
-    }
-
-    public Radio() {
-
-    }
 
 }
